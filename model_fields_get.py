82792e50066d918d6ad2c79eca2b9e65149115cf
xxl4 <nice.lizhi@gmail.com>
2024-12-19 03:30:09 +0000

    update

--- model_fields_get.py
+++ model_fields_get.py
@@ -36,8 +36,32 @@ else:
     model_name = 'product.product'  # Replace with your model name
     model_name = 'sale.order.line'  # Replace with your model name
     model_name = 'product.template'  # Replace with your model name
+    model_name = 'payment.provider'  # Replace with your model name
 
-    print(f"Fields of model: {model_name}")
+    # create a model fields md file if it does not exist
+    # print(f"Creating {model_name}_fields.md")
+    # with open(f'{model_name}_fields.md', 'w') as f:
+    #     f.write(f"#Fields of model: {model_name}\n\n")
+
+    #     # Retrieve the fields of the model
+    #     fields = models.execute_kw(
+    #         db, uid, api_key,
+    #         model_name, 'fields_get',
+    #         [],
+    #         {}
+    #     )
+
+    #     # Write the fields to the file
+    #     for field_name, field_info in fields.items():
+    #         f.write(f"#Field: {field_name}\n\n")
+    #         for attr, value in field_info.items():
+    #             #f.write(f"  {attr}: {value}\n")
+    #             #create a table
+    #             f.write(f"|{attr}|{value}|\n\n")
+    #     f.write("\n\n")
+
+    # print(f"Fields of model: {model_name}")
+    # exit()
 
     # Retrieve the fields of the model
     fields = models.execute_kw(
